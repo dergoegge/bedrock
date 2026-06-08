41b5fd5276737bd9bf2a9d8ca5f21ae1ca6b38cb
Niklas Gögge <n.goeggi@gmail.com>
2026-06-08 18:35:53 +0200

    Harden AWS BIOS boot layout

--- nix/hosts/aws-r7i-metal/disko.nix
+++ nix/hosts/aws-r7i-metal/disko.nix
@@ -6,15 +6,28 @@
     device = lib.mkDefault "/dev/nvme0n1";
     content = {
       type = "gpt";
+      postCreateHook = ''
+        parted /dev/nvme0n1 disk_set pmbr_boot on
+      '';
       partitions = {
         bios = {
           size = "1M";
           type = "EF02";
           priority = 1;
         };
+        boot = {
+          size = "1G";
+          priority = 2;
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            extraArgs = [ "-F" "-L" "boot" ];
+            mountpoint = "/boot";
+          };
+        };
         root = {
           size = "100%";
-          priority = 2;
+          priority = 3;
           content = {
             type = "filesystem";
             format = "ext4";
